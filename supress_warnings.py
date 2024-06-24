01b5c56b034996fc972747cc81a816cc74784c20
FranziskaDigital <franziska.klemstein@hs-mainz.de>
2024-06-24 09:54:59 +0200

    supress_warnings

--- supress_warnings.py
+++ supress_warnings.py
@@ -1,3 +1,6 @@
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module='urllib3')
+#warnings.filterwarnings("ignore", category=UserWarning, module='urllib3')
+from urllib3.exceptions import NotOpenSSLWarning
+
+warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
 warnings.filterwarnings("ignore", category=FutureWarning, module='folium.utilities')
